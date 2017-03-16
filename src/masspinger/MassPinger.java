6d0337cf8089a33fa9e0f8accfcc9252f42548ff
breakbits <breakbits@gmail.com>
2017-03-16 20:38:35 +0500

    start proj

---
+++ src/masspinger/MassPinger.java
@@ -0,0 +1,12 @@
+package masspinger;
+
+/**
+ * Created by kamil on 16.03.2017.
+ */
+public class MassPinger {
+
+           public static void main(String[] args) {
+            System.out.println("Hello World!");
+        }
+
+}
